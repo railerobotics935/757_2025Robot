2882ce99aceee2ce81f2f2ff2e7e09a61a42d444
unknown <team935programming@gmail.com>
2025-03-05 17:13:05 -0600

    Prepared and Documented Pitch PID limits

--- src/main/cpp/subsystems/IntakeSubsystem.cpp
+++ src/main/cpp/subsystems/IntakeSubsystem.cpp
@@ -5,7 +5,6 @@
 #include "subsystems/IntakeSubsystem.h"
 #include "Constants.h"
 
-
 IntakeSubsystem::IntakeSubsystem() 
 : m_intakeSparkMax{IntakeConstants::kIntakeMotorID, IntakeConstants::kMotorType}, 
   m_pitchSparkMax{IntakeConstants::kPitchMotorID, IntakeConstants::kMotorType} {
@@ -22,16 +21,27 @@ void IntakeSubsystem::SetIntakeMotorPower(double power) {
 }
 
 void IntakeSubsystem::SetPitchPosition(units::radian_t setAngle) {
-  // Sets the motor's power (between -1.0 and 1.0).
+  // Use the Spark MAX internal PID controller to reach the setAngle
+  // - downward is counterclockwise along the robot Y-axis => positive direction for Angle
+  // - check intake rotates down when applying a positive value to the SetPitchPower method,
+  //   if this is not the case, invert the Pitch Spark MAX motor controller.
+  // - check absolute encoder on the pitch angle to have an increasing value when rotating downward
+  //   if this is not the case, add minus sign to m_pitchAbsoluteEncoder.GetPosition() references.
   m_pitchPIDController.SetReference(setAngle.value(), rev::spark::SparkLowLevel::ControlType::kPosition);
 
-  // automatically disable control when outside mechanical operating limits
-  
-  // TODO: split in the 2 directions, check motor power and only disable when going past a boundary
+  // Automatically disable control control direction goes outside mechanical operating limits
+  // - IntakeConstants::kMinimumAngle sets the upper pitch limit, this is the lowest angle value
+  // - IntakeConstants::kMaximumAngle sets the downward pitch limit, this is the highest angle value
 
-  if ((m_pitchAbsoluteEncoder.GetPosition() < IntakeConstants::kMinimumAngle) || 
-      (m_pitchAbsoluteEncoder.GetPosition() > IntakeConstants::kMaximumAngle))
-  {
+  // Limit Pitch going too far up
+  if ((m_pitchAbsoluteEncoder.GetPosition() < IntakeConstants::kMinimumAngle) &&
+      (m_pitchSparkMax.Get() < 0.0)) {
+    m_pitchSparkMax.Set(0.0);
+  }
+
+  // Limit Pitch going too far down
+  if ((m_pitchAbsoluteEncoder.GetPosition() > IntakeConstants::kMaximumAngle) &&
+      (m_pitchSparkMax.Get() > 0.0)) {
     m_pitchSparkMax.Set(0.0);
   }
 }
@@ -54,6 +64,7 @@ bool IntakeSubsystem::CoralInIntake() {
 }
 
 void IntakeSubsystem::ConfigureSparkMax() {
+  // Configure the Intake Spark MAX
   rev::spark::SparkMaxConfig intakeSparkMaxConfig{};
 
   intakeSparkMaxConfig
@@ -67,7 +78,8 @@ void IntakeSubsystem::ConfigureSparkMax() {
 
   m_intakeSparkMax.Configure(intakeSparkMaxConfig, rev::spark::SparkMax::ResetMode::kResetSafeParameters, rev::spark::SparkMax::PersistMode::kPersistParameters);
 
-rev::spark::SparkMaxConfig pitchSparkMaxConfig{};
+  // Configure the Pitch Spark MAX
+  rev::spark::SparkMaxConfig pitchSparkMaxConfig{};
 
   pitchSparkMaxConfig
   .VoltageCompensation(RobotConstants::kVoltageCompentationValue)
